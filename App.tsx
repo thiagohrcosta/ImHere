7625f6c6bf74c3ce2a15da9b217252cb4153b606
ThiagoHRCosta <thiagohrcosta86@gmail.com>
2023-01-26 16:05:19 -0300

    adding element style

--- App.tsx
+++ App.tsx
@@ -2,16 +2,32 @@ import { Text, View } from "react-native";
 
 export default function App() {
   return (
-    <View>
+    <View
+      style={{
+        flex: 1,
+        backgroundColor: '#131016',
+        padding: 24
+      }}
+    >
       <Text
         key="1"
+        style={{
+          color: '#FDFCFE',
+          fontSize: 24,
+          fontWeight: 'bold',
+          marginTop: 48
+        }}
       >
-        React Native
+        Nome do evento
       </Text>
       <Text
-        key="1"
+        key="2"
+        style={{
+          color: '#6B6B6B',
+          fontSize: 16
+        }}
       >
-        Rocketseat
+        Quinta, 26 de janeiro de 2023
       </Text>
     </View>
     
