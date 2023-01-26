34c003c6a3f60fd262904c00c1163c3d2cf0627f
ThiagoHRCosta <thiagohrcosta86@gmail.com>
2023-01-26 16:13:42 -0300

    Project separate folder

--- App.tsx
+++ App.tsx
@@ -1,41 +1,8 @@
-import { Text, View, StyleSheet } from "react-native";
+import Home from "./src/screens/Home";
 
 export default function App() {
   return (
-    <View 
-      style={styles.container}
-    >
-      <Text
-        key="1"
-        style={styles.eventName}
-      >
-        Nome do evento
-      </Text>
-      <Text
-        key="2"
-        style={styles.eventDate}
-      >
-        Quinta, 26 de janeiro de 2023
-      </Text>
-    </View>
-    
+    <Home />
   )
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#131016',
-    padding: 24
-  },
-  eventName: {
-    color: '#FDFCFE',
-    fontSize: 24,
-    fontWeight: 'bold',
-    marginTop: 48
-  },
-  eventDate: {
-    color: '#6B6B6B',
-    fontSize: 16
-  }
-});
---
+++ src/screens/Home/index.tsx
@@ -0,0 +1,24 @@
+import { Text, View, StyleSheet } from "react-native";
+import { styles } from "./styles";
+
+export default function Home() {
+  return (
+    <View 
+      style={styles.container}
+    >
+      <Text
+        key="1"
+        style={styles.eventName}
+      >
+        Nome do evento
+      </Text>
+      <Text
+        key="2"
+        style={styles.eventDate}
+      >
+        Quinta, 26 de janeiro de 2023
+      </Text>
+    </View>
+    
+  )
+}
