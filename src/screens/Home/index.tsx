a7c1df9cdb2fb247a5dea7e7a47c9c45d4196ac4
ThiagoHRCosta <thiagohrcosta86@gmail.com>
2023-01-30 21:14:32 -0300

    export default

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -1,7 +1,7 @@
 import { Text, View, StyleSheet } from "react-native";
 import { styles } from "./styles";
 
-export default function Home() {
+export function Home() {
   return (
     <View 
       style={styles.container}
